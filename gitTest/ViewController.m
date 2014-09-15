ed7c81b893246eb04936907c8009ed192f1284b4
Praphin SP <PSP@Praphins-iMac.local>
2014-09-15 10:39:00 +0530

    Start Commit One

--- gitTest/ViewController.m
+++ gitTest/ViewController.m
@@ -16,6 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"App Opens Up");
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
